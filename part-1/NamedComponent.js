90b5a6a6589e8b8bac47e6c23ee0ebee59ea3d38
Ryan Broome <ryan@MacBook-Pro.local>
2023-11-04 03:11:28 -0400

    step 1 completed

---
+++ part-1/NamedComponent.js
@@ -0,0 +1,3 @@
+const NamedComponent = ({ name = "John Smith" }) => {
+  return <p> "My name is : {name}" </p>;
+};
---
+++ part-1/index.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <FirstComponent />
+      <NamedComponent />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
